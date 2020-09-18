7990af9f7fc3ed1ef27313c118ea7d66934858fe
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-09-17 21:56:58 -0700

    Commiting changes

--- app.js
+++ app.js
@@ -5,4 +5,11 @@
 
 const modalBtn = document.querySelector('.modal-btn');
 const modal = document.querySelector('.modal-overlay');
-const closeBtn = document.querySelector('close-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+modalBtn.addEventListener('click', function () {
+  modal.classList.add('open-modal');
+});
+closeBtn.addEventListener('click', function () {
+  modal.classList.remove('open-modal');
+});
